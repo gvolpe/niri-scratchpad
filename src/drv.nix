558830d29c76632529c86abcd3359f154ff4758e
Gabriel Volpe <volpegabriel@gmail.com>
2025-09-13 22:59:23 +0200

    remove libnotify dependency

--- src/drv.nix
+++ src/drv.nix
@@ -1,9 +1,9 @@
-{ jq, libnotify, niri, writeShellApplication }:
+{ jq, niri, writeShellApplication }:
 
 writeShellApplication {
   name = "niri-scratchpad";
   bashOptions = [ "errexit" "pipefail" ];
-  runtimeInputs = [ jq libnotify niri ];
+  runtimeInputs = [ jq niri ];
   text =
     let src = builtins.readFile ./scratchpad.sh;
     in builtins.replaceStrings [ "#! /usr/bin/env bash" ] [ "" ] src;
